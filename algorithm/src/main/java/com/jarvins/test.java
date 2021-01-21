df6f3bc75d820fdf9f0fe868188e98ef4da7e986
jarvins <jarvinsy.outlook.com>
2021-01-21 21:55:51 +0800

    2021.01.21 删除无用内容.

--- algorithm/src/main/java/com/jarvins/test.java
+++ algorithm/src/main/java/com/jarvins/test.java
@@ -1,14 +1,8 @@
 package com.jarvins;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class test {
 
     public static void main(String[] args) {
-        Map<String,Integer> map = new HashMap<>();
-        map.put("1",1);
-        map.computeIfAbsent("2",k -> Integer.parseInt(k) + 1);
-        System.out.println(map);
+        System.out.println(Double.valueOf(Boolean.FALSE.toString()));
     }
 }
